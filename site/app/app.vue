e15d30b84253a402f62c8abfa6f4e33c67ccf0c6
Pieter K de Villiers <pieter@hey.com>
2026-03-15 11:21:00 +0000

    Add EE widget

--- site/app/app.vue
+++ site/app/app.vue
@@ -1,4 +1,28 @@
 <template>
   <NuxtRouteAnnouncer />
   <NuxtPage />
+  <div id="my-ai-chat-widget-container"></div>
 </template>
+
+<script setup>
+useHead({
+  script: [
+    {
+      innerHTML: `window.myAIChatWidgetConfig = {
+        accountId: '8cac84dfc71fd596',
+        apiKey: '248c0fdaecb701d0320c0a04cb74e29bd85a9d2409e7f8bb54e6b6edf3134392',
+        themeColour: "null",
+        buttonText: "Any questions?",
+        widgetTitle: "Explore Barefoot Business",
+        welcomeMessage: "Welcome, how can I help?",
+        optInRequired: "true",
+      };`,
+    },
+    {
+      src: 'https://d31env5c5sjhq3.cloudfront.net/static/expertecho/widget.js',
+      async: true,
+      defer: true,
+    },
+  ],
+})
+</script>
