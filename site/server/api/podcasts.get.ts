07124c7f56d141e90a9ff19b73abcb91fbdd1218
Pieter K de Villiers <pieter@hey.com>
2026-03-18 10:31:09 +0000

    Sort podcasts

--- site/server/api/podcasts.get.ts
+++ site/server/api/podcasts.get.ts
@@ -1,3 +1,5 @@
 import podcasts from '../../app/data/podcasts.json'
 
-export default defineEventHandler(() => podcasts)
+export default defineEventHandler(() =>
+  [...podcasts].sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime())
+)
